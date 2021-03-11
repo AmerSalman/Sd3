8601cdd4f8a4a038a3291cfe79e98de7d26d023b
Amer.Salman <amer.salman@roehampton.ac.uk>
2021-03-11 20:07:48 +0000

    sss,AS

---
+++ students.js
@@ -0,0 +1,14 @@
+// A definition of a student
+class Student {
+    // Student ID
+    id;
+    // Student name
+    name;
+
+    // Creates a new instance (object) of type Student
+    constructor(id, name) {
+        // Set the id and name of the object instance
+        this.id = id;
+        this.name = name;
+    }
+}
